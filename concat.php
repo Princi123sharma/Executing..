fd26bb9ad02260aa410a7001535509a6b8ae95e8
princi <princisharma086@gmail.com>
2024-01-05 02:23:03 +0530

    new update

---
+++ concat.php
@@ -0,0 +1,8 @@
+<?php
+$name="princi";
+$degree="B.tech";
+$age=20;
+
+echo 'My name is ' .$name.'.and i am pursuing ' .$degree.'.and i am ' .$age.' years old.';
+
+?>
---
+++ ifelse.php
@@ -0,0 +1,39 @@
+<?php
+//if-else statements
+// $day="friday";
+// if($day=="sunday"){
+//     echo "today is holiday";
+// }
+// else{
+//     echo "today is not holiday";
+
+// }
+
+
+// $num=12;
+
+// if($num%2==0){
+    
+//     echo "number is even";
+// }
+// else{
+//     echo "number is not even";
+// }
+
+$age=23;
+if($age<=5){
+    echo "he is a kid";
+}
+else if($age<=18){
+    echo "he is a teenager";
+}
+else if($age<=30){
+    echo "he is adult";
+}
+else{
+    echo "he is a old man";
+}
+
+
+
+?>
