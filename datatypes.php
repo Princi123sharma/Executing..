979b4289ed4b3f113e8e97a0732743b4900de42b
princi <princisharma086@gmail.com>
2024-01-05 01:42:29 +0530

    new update

---
+++ datatypes.php
@@ -0,0 +1,62 @@
+<?php
+/*datatypes in php
+1. integer
+2.float
+3.array
+4.string
+5.boolean
+*/
+$num1=3;
+echo $num1;
+
+$num2=4.5;
+echo "</br> $num2";
+
+$first_name="princi";
+echo "</br> $first_name";
+
+$arr=array("princi",123,'CS',75,true);
+echo "</br> $arr[1]";
+echo "</br> $arr[4]";
+
+//addition of two numbers
+$first="princi";
+$last="sharma";
+$name=$first . " ".$last;
+echo "</br> $name";
+
+//airthmetic operators in php
+
+$a=23;
+$b=45;
+$add=$a + $b;
+echo "</br> $add";
+
+$c=45;
+$d=23;
+$sub=$c-$d;
+echo "</br> this is the substraction :$sub";
+
+$e=45;
+$f=3;
+$mul=$e*$f;
+echo "</br> this is the multiplication: $mul";
+
+$g=46;
+$h=2;
+$div=$g/$h;
+echo "</br> this is the division: $div";
+
+$g=46;
+$h=2;
+$mod=$g%$h;
+echo "</br> this is the mod: $mod";
+
+
+
+
+
+
+
+
+?>
---
+++ ques.php
@@ -0,0 +1,17 @@
+<?php
+$num1=3;
+$num2=5;
+
+$powerofnumber=pow(5,3);
+echo $powerofnumber;
+
+$radius=4;
+$pi=3.14;
+$areaofcircle=$pi*pow(4,2);
+// $areaofcircle=$pi*$radius*$radius;
+echo "</br>";
+echo $areaofcircle;
+
+//
+
+?>
